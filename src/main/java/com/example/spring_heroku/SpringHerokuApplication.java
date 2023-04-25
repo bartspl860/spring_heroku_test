fd26cc23bbd8620640888b26cd3a70fa7b030ce3
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2023-04-25 11:28:24 +0200

    getmapping fix

--- src/main/java/com/example/spring_heroku/SpringHerokuApplication.java
+++ src/main/java/com/example/spring_heroku/SpringHerokuApplication.java
@@ -3,15 +3,17 @@ package com.example.spring_heroku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringHerokuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHerokuApplication.class, args);
 	}
 
-	@GetMapping
+	@GetMapping("/")
 	public String get(){
 		return "Hello Jenkins";
 	}
